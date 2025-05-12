c8173355b632bf8f383dc7ec4fe9ee3daf1496a6
E.Chaves <eliaschavez429@gmail.com>
2025-05-12 16:02:45 -0300

    Criando models

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Tarefas(models.Model):
+    title = models.CharField(blank=False, null=False, max_length=255)
+    descricao = models.TextField()
+    creat_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
