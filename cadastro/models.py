fb2dda29f042752c97b72788fc8f474c7f99b456
E.Chaves <eliaschavez429@gmail.com>
2025-05-12 17:55:53 -0300

    Iniciei a criação das views

--- cadastro/models.py
+++ cadastro/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Tarefas(models.Model):
     title = models.CharField(blank=False, null=False, max_length=255)
-    descricao = models.TextField()
+    descricao = models.TextField(blank=False, max_length=500)
     creat_at = models.DateField(auto_now_add=True)
     update_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
